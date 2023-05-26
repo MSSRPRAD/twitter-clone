e2028a3e289a3f531a5909c2a192a48be8afa544
Malladi Pradyumna <f20210367@hyderabad.bits-pilani.ac.in>
2023-05-26 23:45:12 +0530

    Initial Commit for Frontend

---
+++ twitter-frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <center>
+          <h1 className="text-green-600 text-5xl font-bold">
+              GeeksforGeeks
+          </h1> 
+          <b>Tailwind CSS box-border Class</b> 
+          <div className="box-border h-28 w-32 p-4 
+                      border-4 bg-green-500 m4">
+              A Computer Science Portal
+          </div> 
+      </center>
+    </>
+  )
+}
+
+export default App
